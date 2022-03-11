ec9c39034d5ecb3bc6556b8bc2f9792f4cba899f
dhanamg <100270957+dhanamg@users.noreply.github.com>
2022-03-11 20:15:16 +0530

    Add files via upload

---
+++ demo1.php
@@ -0,0 +1,13 @@
+<?php  
+class demo  
+{  
+        private $a= "hello javatpoint";  
+        public function display()  
+        {  
+        echo $this->a;  
+        }  
+}  
+$obj = new demo();  
+    $obj->display();  
+    var_dump($obj);  
+?>  
